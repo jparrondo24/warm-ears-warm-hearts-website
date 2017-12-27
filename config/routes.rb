4dbf6bc809cad1ea05d0b960f161da6063ef09e3
Justin Parrondo <justinparrondo@gmail.com>
2017-12-27 12:53:31 -0500

    Finished basic about page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/' => 'pages#home'
+  get '/about' => 'pages#about'
 
 end
