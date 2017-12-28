86b24fabde0943d12ebabef7c03a6669daa125cc
Justin Parrondo <justinparrondo@gmail.com>
2017-12-27 21:22:57 -0500

    Edit config for heroku deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,9 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-
+  config.serve_static_assets = true
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
+  config.assets.compile = true
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
