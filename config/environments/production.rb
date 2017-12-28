8e6602d63d2a7ec5e1ccd034a6e6d312db87ffb9
Justin Parrondo <justinparrondo@gmail.com>
2017-12-27 21:36:22 -0500

    Fix issues when compiling assets for heroku deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,10 +74,7 @@ Rails.application.configure do
   config.active_support.deprecation = :notify
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
-  config.log_formatter = ::Logger::Formatter.new
-  config.serve_static_assets = true
-  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
-  config.assets.compile = true
+
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
