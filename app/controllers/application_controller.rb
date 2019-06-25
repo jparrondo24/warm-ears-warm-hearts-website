211a8502308ced0e3c50f15240654f972fad3d45
Justin Parrondo <justinparrondo@gmail.com>
2019-06-24 23:44:59 -0400

    Adds fields for first and lastname in the Mailchimp signup form. TODO: Get list ID from from Morles to change from my test list to the official WE,WH list

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,20 @@ class ApplicationController < ActionController::Base
 
   def mailchimp
     puts params[:email]
-    conn = Faraday.new('https://us3.api.mailchimp.com/3.0/lists/c67925dc73/members')
+    conn = Faraday.new('https://us16.api.mailchimp.com/3.0/lists/c67925dc73/members')
+    puts Rails.application.secrets.mail_chimp_api_key
     conn.authorization(:apikey, Rails.application.secrets.mail_chimp_api_key)
     response = conn.post do |req|
       req.body = '{
         "email_address":"'+params[:email]+'",
-        "status": "subscribed"
+        "status": "subscribed",
+        "merge_fields": {
+          "FNAME":"'+params[:first_name]+'",
+          "LNAME":"'+params[:last_name]+'"
+        }
       }'
     end
+    puts response.body
     render json: response.body
   end
 end
